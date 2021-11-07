b2534185155c8f6e13fe1e005375ceb5ac7ef4c5
xtjkisy <92912307+xtjkisy@users.noreply.github.com>
2021-11-08 02:01:32 +0600

    Update index.php

--- index.php
+++ index.php
@@ -4,14 +4,14 @@
     <p>Please fill in this form to create an account.</p>
     <hr>
 
-    <label for="email"><b>Email</b></label>
-    <input type="text" placeholder="Enter Email" name="email" required>
+    <label for="name"><b>Name</b></label>
+    <input type="text" placeholder="Enter Username" name="name" required>
 
     <label for="psw"><b>Password</b></label>
     <input type="password" placeholder="Enter Password" name="psw" required>
 
-    <label for="psw-repeat"><b>Repeat Password</b></label>
-    <input type="password" placeholder="Repeat Password" name="psw-repeat" required>
+   
+   
 
     <label>
       <input type="checkbox" checked="checked" name="remember" style="margin-bottom:15px"> Remember me
