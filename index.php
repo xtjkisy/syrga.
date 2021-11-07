27be1e397caeef849b3be65a2c21de770b56986a
xtjkisy <92912307+xtjkisy@users.noreply.github.com>
2021-11-08 02:02:50 +0600

    Update index.php

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
 
     <div class="clearfix">
       <button type="button" class="cancelbtn">Cancel</button>
-      <button type="submit" class="signupbtn">Sign Up</button>
+      <button type="submit" class="signupbtn">Log In</button>
     </div>
   </div>
 </form>
