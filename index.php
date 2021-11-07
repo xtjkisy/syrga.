e8347269f3e20305edaaf12beec9b7a23e59a3ad
xtjkisy <92912307+xtjkisy@users.noreply.github.com>
2021-11-08 01:49:20 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,25 +1,27 @@
-<form action="action_page.php" method="post">
-  <div class="imgcontainer">
-    <img src="One-Piece.jpg" alt="just" class="one piece">
-  </div>
-
+<form action="dama.php" style="border:1px solid #ccc">
   <div class="container">
-    <label for="uname"><b>Username</b></label>
-    <input type="text" placeholder="syrga.usupova" name="uname" required>
+    <h1>Sign Up</h1>
+    <p>Please fill in this form to create an account.</p>
+    <hr>
+
+    <label for="email"><b>Email</b></label>
+    <input type="text" placeholder="Enter Email" name="email" required>
 
     <label for="psw"><b>Password</b></label>
-    <input type="password" placeholder="usupova123" name="psw" required>
+    <input type="password" placeholder="Enter Password" name="psw" required>
 
-    <button type="submit">Login</button>
+    <label for="psw-repeat"><b>Repeat Password</b></label>
+    <input type="password" placeholder="Repeat Password" name="psw-repeat" required>
+
     <label>
-      <input type="checkbox" checked="checked" name="remember"> Remember me
+      <input type="checkbox" checked="checked" name="remember" style="margin-bottom:15px"> Remember me
     </label>
-  </div>
 
-  <a href="dama.php">linkSyrga</a>
-  
-  <div class="container" style="background-color:#f1f1f1">
-    <button type="button" class="cancelbtn">Cancel</button>
-    <span class="psw">Forgot <a href="#">password?</a></span>
+    <p>By creating an account you agree to our <a href="#" style="color:dodgerblue">Terms & Privacy</a>.</p>
+
+    <div class="clearfix">
+      <button type="button" class="cancelbtn">Cancel</button>
+      <button type="submit" class="signupbtn">Sign Up</button>
+    </div>
   </div>
 </form>
