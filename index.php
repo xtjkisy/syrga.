b1a1bf4716533fec810c624c67d38ccdf05e5ab5
xtjkisy <92912307+xtjkisy@users.noreply.github.com>
2021-11-09 01:51:32 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,30 +1,20 @@
-<html>
-<head></head>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <tittle>Login Page</tittle>
+</head>
+<body bgcolor="purple">
 
-<body bgcolor="yellow">
-    <center><h1>Автобиография.</h1></center>
-<h1>Меня зовут Асанбаева Акылай Ырысбековна, я родилась в 2004 году в  Иссык-Кульской области в селе Теплоключенка.</h1>
-   <style>
-   .rightpic {
-    float: right; 
-    margin: 0 0 5px 5px; 
-   }
-  </style> 
-<img src=akylai.jpeg  
- width="300" height="400" alt="Иллюстрация" align="left" vspace="5" hspace="5">
-    <h1>Моя семья:</h1>
- 
-   <ul>
-       <h2><li>Папа-Асанбаев Ырысбек</li></h2>
-     <h2><li>Мама-Асаналиева Айгуль</li></h2>
-     <h2><li>Брат-Асанбаев Жанат</li></h2>
-     <h2><li>Старшая сестра-Асанбаева Назик</li></h2>
-     <h2><li>Сестра-Асанбаева Бегимай</li></h2>
-     <h2><li>Я-Асанбаева Акылай</li></h2>
-   </ul>
-  
+<form action="/form_login" method="post">
+<h2></h2>Username:<br>
+<input type="text" name='username'><br>
+Password:<br>
+<input name='password' ><br><br>
+<input type="submit" value="Login">
+</form>
+<h1 >{{info}}</h1>
 
-  
-  
 </body>
-<html>
+</html>
+
