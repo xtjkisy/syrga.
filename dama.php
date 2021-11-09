6024a8e0e3032d595128060c441efb3dfcd6d715
xtjkisy <92912307+xtjkisy@users.noreply.github.com>
2021-11-09 20:15:44 +0600

    Update dama.php

--- dama.php
+++ dama.php
@@ -1,16 +1,16 @@
 <html>
 
 <head>
- <title>Usupova Syrga Damirbekovna</title>
-
+ <title>Damirbekovna</title>
 </head>
 <boby>
 <table width="30%" border="0">
  <tr>
    
- <h1 style="font-size:10vw">My portfolio</h1>
+ <h1 style="font-size:10vw">Usupova Syrga</h1>
     
-  <img src="syrga.jpeg"  width="180" height="300">
+  <img src="syrga.jpeg" alt="Иллюстрация" 
+   width="200" height="300" class="rightpic">
  </body>
 </html>
  </td>
