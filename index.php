4d3dcaf026181a6b8dcb20883bcdb10653d018a7
xtjkisy <92912307+xtjkisy@users.noreply.github.com>
2021-11-09 19:53:50 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,26 @@
-<!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <tittle>Login Page</tittle>
+<body bgcolor="yellow">
+
+<title>Login paget</title>
 </head>
-<body bgcolor="purple">
+<script type="text/javascript">
+function display(form){
+if (form.username.value=="root") { 
+  if (form.password.value=="root") {              
+          location="dama.php" 
+           } else {
+             alert("Invalid Password")
+             }
+        } else {  alert("Invalid Username")
+          }
+      }
+   </script>   
+<body >
 
-<form action="/form_login" method="post">
-<h2></h2>Username:<br>
-<input type="text" name='username'><br>
-Password:<br>
-<input name='password' ><br><br>
-<input type="submit" value="Login">
-      <a href="dama.php">
+<form >
+<input type="text" name="username" /><br><br>
+<input type="password" name="password"/><br><br>
+<input type="button" value="Login" onClick="display(this.form)"/>
+
 </form>
-<h1 >{{info}}</h1>
-
-</body>
-</html>
-
