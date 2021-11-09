182fa2033f06502caeb4ad9553ea43d8b044bad6
xtjkisy <92912307+xtjkisy@users.noreply.github.com>
2021-11-09 21:41:27 +0600

    Update dama.php

--- dama.php
+++ dama.php
@@ -3,49 +3,14 @@
 <head>
  <title>Damirbekovna</title>
 </head>
-<boby>
+<body>
 <table width="30%" border="0">
  <tr>
    
  <h1 style="font-size:10vw">Usupova Syrga</h1>
     
   <img src="syrga.jpeg" alt="Иллюстрация" 
-   width="200" height="300" class="rightpic">
+   width="200" height="300" class="leftpic">
  </body>
-</html>
- </td>
 
-  </tr>
-
-</table>
-<br /><br />
-<table width="50%" border="0" cellspacing="15">
-<tr>
-
-    <td> Name, surname - Usupova Syrga. </td>
-
-  </tr>
-
-  <tr>
-
-    <td>Year of birth - 2004. </td>
-
-  </tr>
-
-  <tr>
-
-    <td>Place of birth - Bishkek city. </td>
-
-  </tr> 
-  
-  </tr>
-  
-  <td>Number - 996 700 303 234. </td>
-
-</table>
-
-
-
-</body>
-
-</html>
+ </html>
