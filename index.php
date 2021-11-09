4a17f791c97a764ca75d30d06f62edbda84caa74
xtjkisy <92912307+xtjkisy@users.noreply.github.com>
2021-11-09 21:19:02 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,34 +1,18 @@
 <html>
 <head>
-<body bgcolor="purple">
 
-<title>Login paget</title>
+<title>Login</title>
 </head>
-<script type="text/javascript">
-function display(form){
-if (form.username.value=="syrga") { 
-  if (form.password.value=="123456") {              
-          location="dama.php" 
-           } else {
-             alert("Invalid Password")
-             }
-        } else {  alert("Invalid Username")
-          }
-      }
-   </script>   
-<body >
-<center>
- <h1>
-<form >
- <label for="uname"><b>Username</b></label>
-    <input type="text" placeholder="Enter Username" name="uname">
 
-    <label for="psw"><b>Password</b></label>
-    <input type="password" placeholder="Enter Password" name="psw">
+<body>
 
-    <button type="submit">Login</button>
-    <label>
-      <input type="checkbox" checked="checked" name="remember"> Remember me
-    </label>
-<center>
-<h1>
+<form action="login.php" method="POST" align="center">
+<br>
+Username:<input type="text" name="username"><br><br><br>
+Password :<input type="text" name="password"><br><br>
+
+<input type="Submit"  value="Submit">
+
+</form>
+</body>
+</html>
