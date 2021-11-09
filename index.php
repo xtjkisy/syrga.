d9a837dd35a29df1bb6f2947367a67e9fa5411b5
xtjkisy <92912307+xtjkisy@users.noreply.github.com>
2021-11-09 20:51:10 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <html>
 <head>
-<body bgcolor="purple">
 
 <title>Login paget</title>
-</head>
+  </head>
+   <style>
+	body {
+		background: #000 url(asd.png);
+ </body>
 <script type="text/javascript">
 function display(form){
 if (form.username.value=="syrga") { 
@@ -16,12 +19,19 @@ if (form.username.value=="syrga") {
           }
       }
    </script>   
-<body >
-<center>
- 
-   <form >
-<input type="text" name="username" /><br><br>
-<input type="password" name="password"/><br><br>
-<input type="button" value="Login" onClick="display(this.form)"/>
-<center>
-</form>
+<body>  
+    <center> <h1> Student Login Form </h1> </center> 
+    <form>
+        <div class="container"> 
+            <label>Username : </label> 
+            <input type="text" placeholder="Enter Username" name="username" required>
+            <label>Password : </label> 
+            <input type="password" placeholder="Enter Password" name="password" required>
+            <button type="submit">Login</button> 
+            <input type="checkbox" checked="checked"> Remember me 
+            <button type="button" class="cancelbtn"> Cancel</button> 
+            Forgot <a href="#"> password? </a> 
+        </div> 
+    </form>   
+</body>   
+</html>
