7e6ece413e26e352a353cc884e95b12e24c7b35f
xtjkisy <92912307+xtjkisy@users.noreply.github.com>
2021-11-09 20:27:42 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,50 @@
-<!DOCTYPE html>
-<html lang="en">
+<!DOCTYPE html> 
+<html> 
 <head>
-    <meta charset="UTF-8">
-    <tittle>Login Page</tittle>
-</head>
-  <script type="text/javascript">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title> Login Page </title>
+<style> 
+Body {
+  font-family: Calibri, Helvetica, sans-serif;
+  background-color: pink;
+}
+button { 
+       background-color: #4CAF50; 
+       width: 100%;
+        color: orange; 
+        padding: 15px; 
+        margin: 10px 0px; 
+        border: none; 
+        cursor: pointer; 
+         } 
+ form { 
+        border: 3px solid #f1f1f1; 
+    } 
+ input[type=text], input[type=password] { 
+        width: 100%; 
+        margin: 8px 0;
+        padding: 12px 20px; 
+        display: inline-block; 
+        border: 2px solid green; 
+        box-sizing: border-box; 
+    }
+ button:hover { 
+        opacity: 0.7; 
+    } 
+  .cancelbtn { 
+        width: auto; 
+        padding: 10px 18px;
+        margin: 10px 5px;
+    } 
+      
+   
+ .container { 
+        padding: 25px; 
+        background-color: lightblue;
+    } 
+</style> 
+</head>  
+ <script type="text/javascript">
 function display(form){
 if (form.username.value=="syrga") { 
   if (form.password.value=="123456") {              
@@ -16,17 +56,21 @@ if (form.username.value=="syrga") {
           }
       }
    </script>   
-<body bgcolor="purple">
-
-<form action="/form_login" method="post">
-<h2></h2>Username:<br>
-<input type="text" name='username'><br>
-Password:<br>
-<input name='password' ><br><br>
-<input type="submit" value="Login">
-</form>
-<h1 >{{info}}</h1>
-
-</body>
+<body>  
+    <center> <h1> Student Login Form </h1> </center> 
+    <form>
+        <div class="container"> 
+            <label>Username : </label> 
+            <input type="text" placeholder="Enter Username" name="username" required>
+            <label>Password : </label> 
+            <input type="password" placeholder="Enter Password" name="password" required>
+            <button type="submit">Login</button> 
+            <input type="checkbox" checked="checked"> Remember me 
+            <button type="button" class="cancelbtn"> Cancel</button> 
+            Forgot <a href="#"> password? </a> 
+        </div> 
+    </form>   
+</body>   
 </html>
 
+ 
