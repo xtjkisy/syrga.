d375ea94916b6f29f3e8b55a4ca45e7b21eead83
xtjkisy <92912307+xtjkisy@users.noreply.github.com>
2021-11-09 21:09:17 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,7 @@
-<html>
-<head>
-<body bgcolor="purple">
-
-<title>Login paget</title>
-</head>
-<script type="text/javascript">
+<form action="action_page.php" method="post">
+  <div class="imgcontainer">
+    <body bgcolor="purple">
+      <script type="text/javascript">
 function display(form){
 if (form.username.value=="syrga") { 
   if (form.password.value=="123456") {              
@@ -16,14 +13,28 @@ if (form.username.value=="syrga") {
           }
       }
    </script>   
-<body >
-<center>
- <h1>
-<form >
-<input type="text" name="username" /><br><br>
-<input type="password" name="password"/><br><br>
-<input type="button" value="Login" onClick="display(this.form)"/>
-<center>
-<h1>
+    </body>
+  </div>
+
+  <div class="container">
+    <label for="uname"><b>Username</b></label>
+    <input type="text" placeholder="Enter Username" name="uname" required>
+
+    <label for="psw"><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="psw" required>
+
+    <button type="submit">Login</button>
+    <label>
+      <input type="checkbox" checked="checked" name="remember"> Remember me
+    </label>
+  </div>
+
+  <a href="dama.php">
+    
+  
+  <div class="container" style="background-color:#f1f1f1">
+    <button type="button" class="cancelbtn">Cancel</button>
+    <span class="psw">Forgot <a href="#">password?</a></span>
+  </div>
 </form>
 
