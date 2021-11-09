7c6d479e43d9b00a53a87bb2993cef4a77d2f115
xtjkisy <92912307+xtjkisy@users.noreply.github.com>
2021-11-09 21:15:38 +0600

    Update index.php

--- index.php
+++ index.php
@@ -21,10 +21,10 @@ if (form.username.value=="syrga") {
  <h1>
 <form >
  <label for="uname"><b>Username</b></label>
-    <input type="text" placeholder="Enter Username" name="uname" required>
+    <input type="text" placeholder="Enter Username" name="uname">
 
     <label for="psw"><b>Password</b></label>
-    <input type="password" placeholder="Enter Password" name="psw" required>
+    <input type="password" placeholder="Enter Password" name="psw">
 
     <button type="submit">Login</button>
     <label>
