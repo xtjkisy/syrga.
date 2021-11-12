afc5364271794fb7c3e423b631a3e20af46576ad
xtjkisy <92912307+xtjkisy@users.noreply.github.com>
2021-11-12 14:32:59 +0600

    Update dama.php

--- dama.php
+++ dama.php
@@ -7,7 +7,7 @@
 <table width="30%" border="0">
  <tr>
    
- <h1 style="font-size:10vw">Usupova Syrga</h1>
+ <h1 style="font-size:10vw">Damiirbekova</h1>
     
   <img src="syrga.jpeg" alt="Иллюстрация" 
    width="200" height="300" class="leftpic">
