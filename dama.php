15695a56f14c86994b94e70599cb435dbb23c36a
xtjkisy <92912307+xtjkisy@users.noreply.github.com>
2021-11-12 14:37:50 +0600

    Update dama.php

--- dama.php
+++ dama.php
@@ -9,8 +9,15 @@
    
  <h1 style="font-size:10vw">Damiirbekova</h1>
     
-  <img src="syrga.jpeg" alt="Иллюстрация" 
-   width="200" height="300" class="leftpic">
+  <p><img src="SYRGA1" 
+  width="100" height="200" 
+  alt="Иллюстрация" align="left" 
+  vspace="5" hspace="5">
+  Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diem nonummy nibh 
+  euismod tincidunt ut lacreet dolore magna aliguam erat volutpat. Ut wisis enim 
+  ad minim veniam, quis nostrud exerci tution ullamcorper suscipit lobortis nisl 
+  ut aliquip ex ea commodo consequat.</p> 
+   width="300" height="400" class="leftpic">
  </body>
 
  </html>
