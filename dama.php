0deb688bec90928caa49ae364c4d604f67aa6c71
xtjkisy <92912307+xtjkisy@users.noreply.github.com>
2021-11-12 14:44:05 +0600

    Update dama.php

--- dama.php
+++ dama.php
@@ -10,14 +10,12 @@
  <h1 style="font-size:10vw">Damiirbekova</h1>
     
   <p><img src="SYRGA1" 
-  width="100" height="200" 
+  width="300" height="400" 
   alt="Иллюстрация" align="left" 
   vspace="5" hspace="5">
-  Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diem nonummy nibh 
-  euismod tincidunt ut lacreet dolore magna aliguam erat volutpat. Ut wisis enim 
-  ad minim veniam, quis nostrud exerci tution ullamcorper suscipit lobortis nisl 
-  ut aliquip ex ea commodo consequat.</p> 
-   width="300" height="400" class="leftpic">
+  </p> 
+  <h1>I was born in 13th September 2004, in Bishkek. I went to music school in 2011 to komuz. 
+   2015 i visited Turkey because there was a International festifal.</h1>
  </body>
 
  </html>
