7d29a3766d833de0d9a863577ed1040d0e3622db
xtjkisy <92912307+xtjkisy@users.noreply.github.com>
2021-11-14 20:51:14 +0600

    Update dama.php

--- dama.php
+++ dama.php
@@ -9,25 +9,29 @@
  <tr>
    
 <center><h1> <style="font-size:10vw">Damiirbekova</h1></center>
-  <p><img src="SYRGA1" 
-  width="300" height="400" 
-  alt="Иллюстрация" align="right" 
-  vspace="5" hspace="5">
+  
  <p><h2>I was born on September 13, 2004 in Bishkek.
   </h2></p>
   
   </p> 
- <p></p>
+
   <p><img src="photo5219760925941282320.jpg" 
   width="600" height="500 align="bottom" 
   alt="Иллюстрация" align="center" 
   vspace="5" hspace="5">
    </p>
  
-  <p><h2>In 2011, I started going to music school, 
-   where I began to make good progress.
-   After the next win of our ensemble, we were sent to the annual festival that took place in Turkey.
+  <p><h2>In 2011,I started going to a music school majoring in komuz,
+where I began to make good progress.
+   After the next victory of our ensemble, we were sent to the annual festival, which was held in Turkey.
  </h2></p>
+ <p><img src="photo5219760925941282320.jpg" 
+  width="600" height="500 align="bottom" 
+  alt="Иллюстрация" align="center" 
+  vspace="5" hspace="5">
+   </p>
+ <p><h2>After arriving in Kyrgyzstan, I participated in the World Nomad Games.
+ in 2017, I took first place among the komuzists of the Chui region</h2></p>
  
   
  
