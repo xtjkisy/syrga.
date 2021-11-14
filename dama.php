6c70127cbbd35b6e466fd067fece6f0b47406163
xtjkisy <92912307+xtjkisy@users.noreply.github.com>
2021-11-14 20:42:25 +0600

    Update dama.php

--- dama.php
+++ dama.php
@@ -8,24 +8,26 @@
 <table width="30%" border="0">
  <tr>
    
- <h1> <style="font-size:10vw">Damiirbekova</h1>
-   <p><h1>I was born on September 13, 2004 in Bishkek.
-  </h1></p>
+<center><h1> <style="font-size:10vw">Damiirbekova</h1></center>
   <p><img src="SYRGA1" 
   width="300" height="400" 
   alt="Иллюстрация" align="right" 
   vspace="5" hspace="5">
+ <p><h2>I was born on September 13, 2004 in Bishkek.
+  </h2></p>
+  
   </p> 
+ <p></p>
   <p><img src="photo5219760925941282320.jpg" 
   width="600" height="500 align="bottom" 
   alt="Иллюстрация" align="center" 
   vspace="5" hspace="5">
    </p>
  
-  <p><h1>In 2011, I started going to music school, 
+  <p><h2>In 2011, I started going to music school, 
    where I began to make good progress.
    After the next win of our ensemble, we were sent to the annual festival that took place in Turkey.
- </h1></p>
+ </h2></p>
  
   
  
