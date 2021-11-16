d5524c60c112133f3d22e51f0dfb4cc0bf7807fc
xtjkisy <92912307+xtjkisy@users.noreply.github.com>
2021-11-16 10:44:34 +0600

    Update asd.php

--- asd.php
+++ asd.php
@@ -1,1 +1,26 @@
+<html>
 
+<head>
+ <body bgcolor="beige">
+ <title>Damirbekovna</title>
+</head>
+<body>
+<table width="30%" border="0">
+ <tr>
+   
+<center><h1> <style="font-size:10vw">Damiirbekova</h1></center>
+  
+ <p><h2>I was born on September 13, 2004 in Bishkek.
+  </h2></p>
+ 
+ 
+ <img style="float: right;" alt="" src="photo5226576394794941045.jpg" />
+<div style="clear: right">
+  <p> Name - Syrga</p>
+  <p> Number- 0700303234</p>
+  <p> Place of residence- Housing estate Altyn Ordo </p>
+  
+</div>    
+    
+ </body>
+ </html>
