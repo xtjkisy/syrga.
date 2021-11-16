593b3556bb9b4d79f38b6f4342543033db559968
xtjkisy <92912307+xtjkisy@users.noreply.github.com>
2021-11-16 19:05:48 +0600

    Update asd.php

--- asd.php
+++ asd.php
@@ -9,16 +9,19 @@
  <tr>
  <p style="font-style:italic">font-style:italic</p>  
 <center><h1> <style="font-size:10vw">My portfolio</h1></center>
-  
-
- 
- <img style="float: right; width="300" height="400" align="top" src="photo5231182068615066948.jpg" />
+  <img style="float: right; width="300" height="400" align="top" src="photo5231182068615066948.jpg" />
 <div style="clear: right" >
-  <h1>Name - Syrga
-  <p> Number- 0700303234</p>
-  <p> Place of residence- Housing estate Altyn Ordo </p>
-  <p><a href="https://www.instagram.com/damiirbekovas/"></a></p>
+  <h1>Name: Syrga
+ <p> Surname: Usupova </p>
+ <p> Age: 17</p>
+ <p> Birth Date: 13.09.2004</p>
+ <p>Study: Ala-Too
+  <p> Phone-Number: 0700303234</p>
+  <p> Place of residence: Housing estate Altyn Ordo </p>
+ <p> 
+  
  </h1>
+ <p><a href="https://www.instagram.com/damiirbekovas/"></a></p>
   
 </div>    
     
