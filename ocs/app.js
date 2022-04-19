73147f92021523a8bb6bb303cef614d6dec1a9c4
xtjkisy <92912307+xtjkisy@users.noreply.github.com>
2022-04-19 16:56:57 +0600

    Create app.js

---
+++ ocs/app.js
@@ -0,0 +1,10 @@
+function display(form){ 
+    if (form.username.value=="syrga") {  
+      if (form.password.value=="123456") {               
+              location="asd.php"  
+               } else { 
+                 alert("Invalid Password") 
+                 } 
+            } else {  alert("Invalid Username") 
+              } 
+          } 
