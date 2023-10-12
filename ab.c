3a86c6090a94ca1eb57edfdada6b88a5f4ed29d0
xtjkisy <92912307+xtjkisy@users.noreply.github.com>
2023-10-12 09:54:31 +0900

    Create ab.c

---
+++ ab.c
@@ -0,0 +1,15 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    char name[] = {'S', 'A', 'N', 'J', 'A', 'Y'};
+    char text[] = "PRABHAKAR";
+    printf("Name:%s\n",name);
+    printf("%s\n", text);
+    printf("%.5s\n", text);
+    printf("%.8s\n", text);
+    printf("%11s\n", text);
+
+    return 0;
+}
